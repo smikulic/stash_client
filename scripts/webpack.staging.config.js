a1ee993c10a86e141ae4937bd8e6089545ef41b1
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2017-04-23 16:26:03 +0200

    Setup staging and production webpack

--- scripts/webpack.staging.config.js
+++ scripts/webpack.staging.config.js
@@ -4,21 +4,29 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, '../dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        'NODE_ENV': JSON.stringify('staging')
       }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+          screw_ie8: true,
+          keep_fnames: true
+      },
+      compress: {
+          screw_ie8: true
+      },
+      comments: false
     })
   ],
   resolve: {
@@ -38,7 +46,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loaders: ['babel'],
-        include: path.join(__dirname, 'src')
+        include: path.join(__dirname, '../src')
       }
     ]
   }
