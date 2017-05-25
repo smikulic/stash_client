d6bde40a026356004a797c4b4eb2a1b430c37577
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2017-05-25 22:23:29 +0200

    Add new font & update dashboard design, super awesome (#8)
    * Add new font & update dashboard design, super awesome
    
    * Fix lint errors

--- src/components/SavingGoals.jsx
+++ src/components/SavingGoals.jsx
@@ -23,11 +23,15 @@ class SavingGoals extends Component {
 
   render() {
     return (
-      <Table style={{background: '#F5F5F5'}}>
+      <Table style={{
+        background: '#F5F5F5',
+        fontFamily: '"Khula", sans-serif',
+        fontWeight: '300',
+      }}>
         <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
           <TableRow>
-            <TableHeaderColumn colSpan="4">Goal</TableHeaderColumn>
-            <TableHeaderColumn colSpan="1">Saved</TableHeaderColumn>
+            <TableHeaderColumn colSpan="3">Goal</TableHeaderColumn>
+            <TableHeaderColumn colSpan="3">Saved</TableHeaderColumn>
             <TableHeaderColumn colSpan="1">Monthly</TableHeaderColumn>
           </TableRow>
         </TableHeader>
@@ -37,20 +41,53 @@ class SavingGoals extends Component {
               this.savingGoals.map((item) => {
                 let durationMonthly = moment(item.end_date).diff(moment(item.start_date), 'months');
                 let monthly = Math.round(item.total_value / durationMonthly);
+                let durationSince = moment().diff(moment(item.start_date), 'months');
                 let due = moment(item.end_date).subtract(1, 'month').endOf('month').fromNow();
                 let percentage = monthly / item.total_value * 100;
-                let saved = moment().diff(moment(item.start_date), 'months') * percentage;
+                let saved = durationSince * percentage;
+                let durationTillEnd = moment(item.end_date).diff(moment(), 'months');
 
-                due = moment(item.end_date).diff(moment(), 'months') === 0 ? 'this month' : due;
+                due = durationTillEnd === 0 ? 'this month' : due;
+
+                let progressBarsCells = [];
+                for (var i = 0; i < 10; i++) {
+                  if (i < Math.round(10 * saved.toFixed(2) / 100)) {
+                    progressBarsCells.push(
+                      <li
+                        key={`progress-bar--filled-cell${i}`}
+                        className="progress-bar--filled-cell"
+                        >
+                        <div className="cell"></div>
+                      </li>
+                    );
+                  } else {
+                    progressBarsCells.push(
+                      <li
+                        key={`progress-bar--empty-cell${i}`}
+                        className="progress-bar--empty-cell"
+                        >
+                        <div className="cell"></div>
+                      </li>
+                    );
+                  }
+                }
 
                 return (
-                  <TableRow key={item.id} style={{height: '6rem'}}>
-                    <TableRowColumn colSpan="4">
+                  <TableRow key={item.id} style={{
+                    height: '6rem',
+                    fontFamily: '"Khula", sans-serif',
+                    fontWeight: '300',
+                  }}>
+                    <TableRowColumn colSpan="3">
                       <div className="table-row--name">{item.name}</div>
                       <div className="table-row--due"><span className="circle"></span>{due}</div>
                     </TableRowColumn>
-                    <TableRowColumn colSpan="1">
-                      <div className="table-row--saved">{saved.toFixed(2)}%</div>
+                    <TableRowColumn colSpan="3">
+                      <div className="table-row--saved">
+                        <ul className="progress">
+                        {progressBarsCells}
+                        </ul>
+                      </div>
                     </TableRowColumn>
                     <TableRowColumn colSpan="1">
                       <div className="table-row--monthly">{monthly} {symbolFromCurrency(this.currency)}</div>
