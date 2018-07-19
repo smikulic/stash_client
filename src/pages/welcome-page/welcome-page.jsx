666f700c00f8fcc777b2c9ec7b9924baf360f7fa
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2018-07-18 23:12:05 -0400

    Add product demo section

--- src/pages/welcome-page/welcome-page.jsx
+++ src/pages/welcome-page/welcome-page.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Carousel from 'nuka-carousel';
 import amplitude from 'amplitude-js/amplitude';
 import FeatureBox from '../../components/feature-box';
 
 class WelcomePage extends Component {
   render() {
     amplitude.getInstance().logEvent('Page load: Welcome Page');
+    var sliderSettings = {
+      dragging: true,
+      speed: 500,
+      cellSpacing: 20,
+      edgeEasing: 'easeOutCirc',
+    };
 
     return (
       <div>
@@ -26,6 +33,13 @@ class WelcomePage extends Component {
           <FeatureBox title="ScroogeVault is 100% free."
             description="Don't worry, there are no fees involved and no bank accounts information needed, we are just offering a glorified calculator and a way to unload the burden of financial planning."
           />
+          <FeatureBox>
+            <Carousel {...sliderSettings}>
+              <img src="../../assets/scroogevault_demo_1.png" alt="Scroogevault Demo"/>
+              <img src="../../assets/scroogevault_demo_2.png" alt="Scroogevault Demo"/>
+              <img src="../../assets/scroogevault_demo_3.png" alt="Scroogevault Demo"/>
+            </Carousel>
+          </FeatureBox>
           <FeatureBox>
             <Link to="/login" className="btn-get-started">Get Started</Link>
           </FeatureBox>
